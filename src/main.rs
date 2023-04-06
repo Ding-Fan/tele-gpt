4ea25ddf4c3fb21b53f94640fb8bf2a522c21f92
Ding Fan <ouud@fastmail.com>
2023-04-06 21:17:41 +0900

    :tada: sic parvis magna

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use dotenvy::dotenv;
+use std::env;
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    // load environment variables from .env file
+    dotenv().expect(".env file not found");
+
+    // for (key, value) in env::vars() {
+    //     println!("{key}: {value}");
+    // }
+
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
+}
